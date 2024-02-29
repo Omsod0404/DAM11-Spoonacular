0e3c5495ee463ce909964cff8eacc3776aab63b5
Carl0B <cbarrera18@alumnos.uaq.mx>
2024-02-29 12:50:24 -0600

    Mi primer commit
    Agregué los colores y fuentes

--- App.jsx
+++ App.jsx
@@ -1,6 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+const colorPalette = {
+  green : 'rgba(0, 66, 37, 1)', /*Hex  #004225*/ 
+  whitelight: 'rgba(245, 245, 220, 1)', /*Hex  #F5F5DC*/
+  yellow: 'rgba(255, 176, 0, 1)', /*Hex  #FFB000*/
+  pinklight: 'rgba(255, 207, 157, 1)', /*Hex  #FFCF9D*/
+};
+
 export default function App() {
   return (
     <View style={styles.container}>
