9a7ecf98287408870f8f737c7e73aa1682b2b5c7
Omsod0404 <garciaomarucho@gmail.com>
2024-02-29 15:41:06 -0600

    Welcome screen finished

---
+++ screen/welcome.jsx
@@ -0,0 +1,59 @@
+import React, {useEffect, useState} from 'react';
+import { StyleSheet, Text, View, SafeAreaView, StatusBar, FlatList, TextInput, TouchableOpacity, Image } from 'react-native';
+import { useNavigation } from '@react-navigation/core';
+import { useFonts } from 'expo-font';
+
+const colorPalette = {
+    green : 'rgba(0, 66, 37, 1)', /*Hex  #004225*/ 
+    whitelight: 'rgba(245, 245, 220, 1)', /*Hex  #F5F5DC*/
+    yellow: 'rgba(255, 176, 0, 1)', /*Hex  #FFB000*/
+    pinklight: 'rgba(255, 207, 157, 1)', /*Hex  #FFCF9D*/
+  };
+
+const WelcomeScreen = () => {
+    //Object for can use Navigation
+    const navigation = useNavigation();
+    //Set a timeout to execute navigation to serach when the timer has reache 0 
+    useEffect(() => {
+        const timeout = setTimeout(navigateToSearch, 2000);
+        return () => clearTimeout(timeout)//To execute the timer
+    }, []);
+    //Function to navigate to search
+    const navigateToSearch = () => {
+        navigation.navigate('Search')
+    };
+    //Fonts used on this screen
+    const [fontsLoaded] = useFonts({
+        'Inter-ExtraBold': require('../assets/fonts/Inter-ExtraBold.ttf')
+    });
+
+    return(
+        <SafeAreaView style={welcomeScreenStyles.container}>
+            {/* Logo */}
+            <Image source={require('../assets/icons/comer.png')} style={welcomeScreenStyles.logo}/>
+            <Text style={welcomeScreenStyles.title}>Dely-Meals</Text>
+        </SafeAreaView>
+    );
+};
+
+const welcomeScreenStyles = StyleSheet.create({
+    container: {
+        backgroundColor: colorPalette.green,
+        flex: 1,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    title:{
+        fontFamily: 'Inter-ExtraBold',
+        color: colorPalette.whitelight,
+        fontSize: 60,
+    },
+    logo:{
+        width: 300,
+        height: 300,
+        resizeMode: 'contain'
+    }
+});
+
+export default WelcomeScreen;
