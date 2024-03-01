7f6aecee6224daf5bb25cc2ea4a9733f54e05e0c
Gera <gr.misterg@gmail.com>
2024-02-29 20:02:57 -0600

    Working on search logic
    Im implementing logic realted to buttons for searching recipes

--- screens/search.jsx
+++ screens/search.jsx
@@ -1,40 +1,52 @@
 import React, {useState, useEffect} from "react";
-import { Text, View, FlatList, Image, StyleSheet, TextInput} from "react-native";
+import { Text, View, FlatList, Image, StyleSheet, TextInput, TouchableOpacity} from "react-native";
 
 export default function Search ()
 {
   const [ingredients, setIngredients] = useState(null);
   const [areas, setAreas] = useState(null);
   const [categories, setCategories] = useState(null);
+  const [imActive, setImActive] = useState([false, false]);
 
-  // For getting the data for filters on loading screen 
-  useEffect(
-    async () => {
+  // For getting the data for filters on loading screen (Only once)
+  useEffect(() => {
+
+    const fetchData = async () => { // Define an async function
       
-        // try to get all data for searching 
-        try {
-          
-          let response_ingredients = await fetch('www.themealdb.com/api/json/v1/1/list.php?i=list');
-          setIngredients(response_ingredients.json());
+      try {
+        let response_ingredients = await fetch('https://www.themealdb.com/api/json/v1/1/list.php?i=list');
+        let data_ingredients = await response_ingredients.json();
+        setIngredients(data_ingredients);
+  
+        let response_areas = await fetch('https://www.themealdb.com/api/json/v1/1/list.php?a=list');
+        let data_areas = await response_areas.json();
+        setAreas(data_areas);
+  
+        let response_categories = await fetch('https://www.themealdb.com/api/json/v1/1/list.php?c=list');
+        let data_categories = await response_categories.json();
+        setCategories(data_categories);
+      } catch (error) {
+        setIngredients(null);
+        setAreas(null);
+        setCategories(null);
+        console.log(error);
+      }
 
-          let response_areas = await fetch('www.themealdb.com/api/json/v1/1/list.php?a=list');
-          setAreas(response_areas.json());
+    };
+  
+    fetchData(); 
+  }, []);
 
-          let response_categories = await fetch('www.themealdb.com/api/json/v1/1/list.php?c=list');
-          setCategories(response_categories.json());
-
-        } catch (e) {
-          setIngredients(null);
-          setAreas(null);
-          setCategories(null);
-          console.log(e);
-        }
-
-    }
-  , []);
+  const  display = (state, index) => {
+    states = [false, false];
+    states[index] = !state;
+    setImActive(states);
+  };
 
   return (
     <View>
+      <Text>{ingredients && areas? 'S': 'N'}</Text>
+      
       <View><Text>Bar</Text></View>
       <View>
         <Text>What you wanna it?</Text>
@@ -45,13 +57,65 @@ export default function Search ()
         </View>
         <View>
           <Text>Search by Ingredient:</Text>
-          <Text>{ingredients}</Text>
+          <TextInput
+            multiline = {false}
+            maxLength={40}/>
         </View>
+        
         <View>
-          <Text>Search by Geographical Area:</Text>
+          {areas && 
+            <>
+              <View>
+                <TouchableOpacity 
+                  onPress={() => display(imActive[0], 0)}>
+                  <Text>Search by Geographical Area:</Text>
+                </TouchableOpacity>
+              </View>
+              {(areas && imActive[0]) &&
+                <View>
+                  <FlatList
+                    data={areas.meals}
+                    persistentScrollbar = {true}
+                    keyExtractor={(item, index) => index.toString()}
+                    renderItem={({ item }) => (
+                      <TouchableOpacity>
+                        <Text>{item.strArea}</Text>
+                      </TouchableOpacity>
+                    )}
+                    style={{ height: 300, width: 300 }}
+                  />
+                </View>
+              }
+            </>
+          }
         </View>
+        
         <View>
-          <Text>Search by Category:</Text>
+          {categories && 
+            <>
+              <View>
+                <TouchableOpacity
+                  onPress={() => display(imActive[1], 1)}>
+                  <Text>Search by Category:</Text>
+                </TouchableOpacity>
+              </View>
+              {(categories && imActive[1]) && 
+                <View>
+                  <FlatList
+                    data={categories.meals}
+                    persistentScrollbar = {true}
+                    keyExtractor={(item, index) => index.toString()}
+                    renderItem={({ item }) => (
+                      <TouchableOpacity>
+                        <Text>{item.strCategory}</Text>
+                      </TouchableOpacity>
+                    )}
+                    style={{ height: 300, width: 300 }}
+                  />
+                </View>
+              }
+            </>
+          }
         </View>
       </View>
     </View>  
